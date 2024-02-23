9dbf97a2422f1afe3300eca8dacdd6e997f35069
Laziz <lazizjonweb@gmail.com>
2024-02-23 09:36:36 +0500

    data objects file is added

---
+++ src/data.js
@@ -0,0 +1,61 @@
+import Project1 from "./assets/brossia.jpg";
+import Project2 from "./assets/chobham.jpg";
+import Project3 from "./assets/combines.jpg";
+import Project4 from "./assets/innovation.jpg";
+import Project5 from "./assets/nordic.jpg";
+import Project6 from "./assets/sample.jpg";
+import Project7 from "./assets/hidden-agenda.jpg";
+
+export const latestProjects = [
+    {
+        title: "Brossia Heigh Luxury Apartment",
+        category: "Residenital",
+        image: Project1,
+        path: "/",
+    },
+    {
+        title: "Chobham Manor Masterplan",
+        category: "Suitantable Space",
+        image: Project2,
+        path: "/",
+    },
+    {
+        title: "Combines Design With Nature",
+        category: "Commercial",
+        image: Project3,
+        path: "/",
+    },
+    {
+        title: "Innovation Private Residence",
+        category: "Suitantable Space",
+        image: Project4,
+        path: "/",
+    },
+    {
+        title: "Inspired From China In Web Design",
+        category: "Commercial",
+        image: Project1,
+        path: "/",
+    },
+    {
+        title: "Nordic Interior Studio",
+        category: "Copper Brass",
+        image: Project5,
+        path: "/",
+    },
+    {
+        title: "Sample Villa",
+        category: "Commercial",
+        image: Project6,
+        path: "/",
+    },
+    {
+        title: "The Hidden Agenda Of Residenital",
+        category: "Residenital",
+        image: Project7,
+        path: "/",
+    },
+];
+
+
+

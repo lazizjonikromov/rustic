aceeb539767835acb0196dd60cb7a24505980065
Laziz <lazizjonweb@gmail.com>
2024-02-23 09:37:15 +0500

    Latest Project has been updated

--- src/pages/main/LatestProjects.jsx
+++ src/pages/main/LatestProjects.jsx
@@ -2,13 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
-import Project1 from "../../assets/brossia.jpg";
-import Project2 from "../../assets/chobham.jpg";
-import Project3 from "../../assets/combines.jpg";
-import Project4 from "../../assets/innovation.jpg";
-import Project6 from "../../assets/nordic.jpg";
-import Project7 from "../../assets/sample.jpg";
-import Project8 from "../../assets/hidden-agenda.jpg";
+import { latestProjects } from "../../data";
 
 const LatestProjects = () => {
   return (
@@ -45,101 +39,19 @@ const LatestProjects = () => {
           modules={[Pagination]}
           className="mySwiper"
         >
-          <SwiperSlide>
-            <Link to={"/"} className="project__item">
-              <div className="project__img">
-                <img src={Project1} alt="" />
-              </div>
-              <div className="project__content">
-                <span>Residenital</span>
-                <h2>Brossia Heigh Luxury Apartment</h2>
-              </div>
-            </Link>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Link to={"/"} className="project__item">
-              <div className="project__img">
-                <img src={Project2} alt="" />
-              </div>
-              <div className="project__content">
-                <span>Suitantable Space</span>
-                <h2>Chobham Manor Masterplan</h2>
-              </div>
-            </Link>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Link to={"/"} className="project__item">
-              <div className="project__img">
-                <img src={Project3} alt="" />
-              </div>
-              <div className="project__content">
-                <span>Commercial</span>
-                <h2>Combines Design With Nature</h2>
-              </div>
-            </Link>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Link to={"/"} className="project__item">
-              <div className="project__img">
-                <img src={Project4} alt="" />
-              </div>
-              <div className="project__content">
-                <span>Suitantable Space</span>
-                <h2>Innovation Private Residence</h2>
-              </div>
-            </Link>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Link to={"/"} className="project__item">
-              <div className="project__img">
-                <img src={Project1} alt="" />
-              </div>
-              <div className="project__content">
-                <span>Commercial</span>
-                <h2>Inspired From China In Web Design</h2>
-              </div>
-            </Link>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Link to={"/"} className="project__item">
-              <div className="project__img">
-                <img src={Project6} alt="" />
-              </div>
-              <div className="project__content">
-                <span>Copper Brass</span>
-                <h2>Nordic Interior Studio</h2>
-              </div>
-            </Link>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Link to={"/"} className="project__item">
-              <div className="project__img">
-                <img src={Project7} alt="" />
-              </div>
-              <div className="project__content">
-                <span>Commercial</span>
-                <h2>Sample Villa</h2>
-              </div>
-            </Link>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Link to={"/"} className="project__item">
-              <div className="project__img">
-                <img src={Project8} alt="" />
-              </div>
-              <div className="project__content">
-                <span>Residenital</span>
-                <h2>The Hidden Agenda Of Residenital</h2>
-              </div>
-            </Link>
-          </SwiperSlide>
+          {latestProjects && latestProjects.map((item, i) => (
+            <SwiperSlide key={i}>
+              <Link to={item.path} className="project__item">
+                <div className="project__img">
+                  <img src={item.image} alt={item.title} />
+                </div>
+                <div className="project__content">
+                  <span>{item.category}</span>
+                  <h2>{item.title}</h2>
+                </div>
+              </Link> 
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>

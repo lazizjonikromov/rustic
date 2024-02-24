0ddc998a5776e63d1106b25f64f48f8c4c464613
Laziz <lazizjonweb@gmail.com>
2024-02-24 11:00:37 +0500

    Navbar is updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from '../assets/logo.png';
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+  const location = useLocation();
+
+  const changeNavbar = () => {
+      if (window.scrollY >= 30) {
+          setNavbar(true);
+      } else {
+          setNavbar(false)
+      }
+  }
+
+  window.addEventListener('scroll', changeNavbar);
+
   return (
-    <div className="navBar w-full z-10">
+    <div className={`navBar w-full z-10 ${navbar ? 'active' : ''}`}>
       <div className="container">
         <div className="row">
           <div className="col-6 col-sm-8 col-lg-2">
-            <a href="/">
+            <Link to={"/"}>
               <img src={logo} alt="" />
-            </a>
+            </Link>
           </div>
           <div className="col-md-7 d-none d-lg-flex">
             <ul className="nav-menu d-flex gap-5 align-items-center">
-              <li className="active">
-                <a href="/">Home</a>
+              <li className={`${location.pathname === '/' ? 'active' : ''}`}>
+                <Link to={"/"}>Home</Link>
               </li>
-              <li>
-                <a href="/about">About</a>
+              <li className={`${location.pathname === '/about' ? 'active' : ''}`}>
+                <Link to={"/about"}>About</Link>
               </li>
-              <li>
-                <a href="/">Projects</a>
+              <li className={`${location.pathname === '/projects' ? 'active' : ''}`}>
+                <Link to={"/"}>Projects</Link>
               </li>
-              <li>
-                <a href="/">Posts</a>
+              <li className={`${location.pathname === '/posts' ? 'active' : ''}`}>
+                <Link to={"/"}>Posts</Link>
               </li>
-              <li>
-                <a href="/">Contact</a>
+              <li className={`${location.pathname === '/contact' ? 'active' : ''}`}>
+                <Link to={"/"}>Contact</Link>
               </li>
             </ul>
           </div>
           <div className="col-6 col-sm-4 col-lg-3 d-flex lang__burg">
             <ul className="d-flex gap-3 gap-sm-5">
               <li>
-                <a href="/">End</a>
+                <Link to={"/"}>End</Link>
               </li>
               <li>
-                <a href="/">Fra</a>
+                <Link to={"/"}>Fra</Link>
               </li>
             </ul>
 

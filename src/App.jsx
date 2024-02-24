4d5c455ff992dfd9a057ad291f5c069fbec3eed3
Laziz <lazizjonweb@gmail.com>
2024-02-24 11:05:20 +0500

    ScrollToTop is added

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Navbar from "./components/Navbar"
 import AboutUs from "./pages/about-us/AboutUs"
 import Main from "./pages/main/Main"
 import Footer from "./components/Footer"
+import ScrollToTop from "./components/ScrollToTop"
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Main />}/>
         <Route path="/about" element={<AboutUs />}/>
       </Routes>
+
       <Footer />
     </>
   )
